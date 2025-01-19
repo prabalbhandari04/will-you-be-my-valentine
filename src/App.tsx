7908b450116a8e3d53fd8b39fc930f6fdfc1fea2
Prabal Bhandari <prabalb046348@gmail.com>
2025-01-19 20:22:50 +0530

    chnages

--- src/App.tsx
+++ src/App.tsx
@@ -16,17 +16,17 @@ export default function Page() {
       "Are you sure?",
       "What if I asked really nicely?",
       "Pretty please",
-      "With a chocolate rice cake on top",
-      "What about a matcha frostie",
+      "With a blueberry cheesecake on top",
+      "What about a strawberry smoothie?",
       "PLEASE POOKIE",
       "But :*(",
       "I am going to die",
-      "Yep im dead",
-      "ok ur talking to nathan's ghost",
-      "please babe",
+      "Yep, I'm dead",
+      "Ok, you're talking to Prabal's ghost",
+      "Please babe",
       ":((((",
       "PRETTY PLEASE",
-      "Estoy muerto",
+      "Tusi Rabb de vaste ha boldeh kudi",
       "No :(",
     ];
 
@@ -34,22 +34,31 @@ export default function Page() {
   };
 
   return (
-    <div className="-mt-16 flex h-screen flex-col items-center justify-center">
+    <div className="flex h-screen flex-col items-center justify-center px-4 text-center">
       {yesPressed ? (
         <>
-          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <img
+            src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif"
+            alt="Bear kiss"
+            className="w-full max-w-sm"
+          />
+          <div className="my-4 text-2xl font-bold sm:text-4xl">
+            WOOOOOO!!! I love you pookie!! ;))
+          </div>
         </>
       ) : (
         <>
           <img
-            className="h-[200px]"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+            alt="Cute love bear"
+            className="w-full max-w-xs sm:max-w-sm"
           />
-          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
-          <div className="flex items-center">
+          <h1 className="my-4 text-2xl sm:text-4xl">
+            Will you be my Valentine?
+          </h1>
+          <div className="flex flex-col items-center sm:flex-row sm:space-x-4">
             <button
-              className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
+              className="mb-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700 sm:mb-0"
               style={{ fontSize: yesButtonSize }}
               onClick={() => setYesPressed(true)}
             >
@@ -57,7 +66,7 @@ export default function Page() {
             </button>
             <button
               onClick={handleNoClick}
-              className=" rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
+              className="rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
             >
               {noCount === 0 ? "No" : getNoButtonText()}
             </button>
