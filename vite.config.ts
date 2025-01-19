6b3052630ab65d582a3700dcc3c37ce734628934
Prabal Bhandari <prabalb046348@gmail.com>
2025-01-19 20:50:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/will-you-be-my-valentine",
+  base: '/', // Ensure assets are correctly referenced
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure the output directory is set correctly
+    assetsDir: 'assets', // You can change this if you want a custom folder for assets
+  },
 });
